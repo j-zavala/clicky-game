6f56d7d6d34c927faa5168a04a6fde6d0c5f85db
Johnny Zavala <jzavalafgz@gmail.com>
2018-11-10 14:28:54 -0500

    added images and content to app files

--- mt-app/src/index.js
+++ mt-app/src/index.js
@@ -1,12 +1,8 @@
+//imports dependencies and files
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//renders the App.js file to the root element (index.html in public folder)
+ReactDOM.render(<App />, document.getElementById("root"));
