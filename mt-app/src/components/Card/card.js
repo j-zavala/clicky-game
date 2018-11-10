2e3803d7c0dedb5bed9b068ce7e24251a55e4d90
Johnny Zavala <jzavalafgz@gmail.com>
2018-11-10 13:46:16 -0500

    added components

---
+++ mt-app/src/components/Card/card.js
@@ -0,0 +1,14 @@
+//sets up the reusable Card component
+import React from "react";
+import "./card.css";
+
+//pass the image into each card so all 12 are rendered
+const Card = props => (
+    <div className="card" onClick={props.imageClick}>
+        <div className="img-container">
+            <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
+        </div>
+    </div>
+);
+
+export default Card;
---
+++ mt-app/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <header className="footer">
+        <p>GAME BY Johnny</p>
+    </header>
+);
+
+export default Footer;
