59cf6e4b392db4ca8b6b08b1715aed1d8afed817
Johnny Zavala <jzavalafgz@gmail.com>
2018-11-10 14:35:06 -0500

    added images to character json file

--- mt-app/src/components/Card/card.js
+++ mt-app/src/components/Card/card.js
@@ -6,7 +6,7 @@ import "./card.css";
 const Card = props => (
     <div className="card" onClick={props.imageClick}>
         <div className="img-container">
-            <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
+            <img alt={props.image.replace(".png", "")} src={require("../../images" + props.image)} />
         </div>
     </div>
 );
